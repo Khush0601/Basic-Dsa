4baa34dd221038e4883a8aedbb0cc46366a94435
khushboo <ksingh200601@gmail.com>
2024-07-30 23:36:49 +0530

    average question done

---
+++ Average.js
@@ -0,0 +1,10 @@
+let a=[1,2,3,4,56,7,8]
+let sum=0
+function add(b){
+  for(let i=0;i<b.length;i++){
+    sum=sum+b[i]
+  }
+let Average=sum/b.length
+ return {sum,Average}
+}
+console.log((add(a)))
