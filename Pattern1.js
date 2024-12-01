f133eed2a678a238194223f3fa373da4ad82a127
khushboo kumari <ksingh200601@gmail.com>
2024-12-01 17:28:39 +0530

    star pattern 1

---
+++ Pattern1.js
@@ -0,0 +1,7 @@
+// pattern 1
+for(let i=0;i<8;i++){
+    for(let j=0;j<=i;j++){
+        process.stdout.write("*"); // ye row m sare start ko le aayaga
+    }
+    process.stdout.write("\n"); // ye column m krega
+}
