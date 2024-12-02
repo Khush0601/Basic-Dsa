7cb434ad3a8f04080dbc1d82429ea3da6c8d979b
khushboo kumari <ksingh200601@gmail.com>
2024-12-02 22:03:00 +0530

    frequencyOfletters in a string done

---
+++ frequecyOfLetter.js
@@ -0,0 +1,33 @@
+// to count the frequency of each letter in a string
+function countFrequency(str){
+    let obj={}
+    let strLowerCase=str.toLowerCase()
+     
+    for(let i=0;i<str.length;i++){
+       let char=str[i]
+       if(obj[char]){
+        obj[char]++
+    }
+    else{
+        obj[char]=1
+    }
+    }
+   
+    
+
+   return obj
+}
+console.log(countFrequency('hellooooyyytttrrrefggg'))
+
+function countFrequencyy(str){
+    let count=1
+    for(word of str){
+        if(obj[word]){
+            obj[word]=count++
+        }
+        else{
+            obj[word]=count
+        }
+    }  
+}
+console.log(countFrequency('shanusingh'))
