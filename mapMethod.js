88257cd080ce24541013f2fc701b963af4af28d2
khushboo kumari <ksingh200601@gmail.com>
2024-12-06 20:05:38 +0530

    map method

--- mapMethod.js
+++ mapMethod.js
@@ -1,14 +1,15 @@
 // creating map:
-
 const person=new Map()
 //set(value)
 person.set('name','khush')
 person.set(10,true)
 //get(value)
-// console.log(person.get('name'))
+console.log(person.get('name'))
 
-// console.log(person.delete(10)) // if agr ye console krnge toh ye true or false return krega that haa delete ho gya
+console.log(person.delete(10)) // if agr ye console krnge toh ye true or false return krega that haa delete ho gya
 console.log(person.entries())
 console.log(person) // isme updated value dega delete krke
 
 
+// keys
+console.log(person.keys())
