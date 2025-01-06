ebee3b00f9d032fb52e77a153aa9a1f6367998cc
khushboo kumari <ksingh200601@gmail.com>
2025-01-06 20:10:11 +0530

    hackerrank question done

---
+++ HackerRankTYpe42ViralAdvertising.js
@@ -0,0 +1,17 @@
+function viralAdvertising(n) {
+    let sharedPeople=5;
+    let likedPeople=0;
+    let cummulative=0;
+    let likedPeopleArr=[]
+
+  for(let i=1;i<=n;i++){
+   likedPeople=Math.floor(sharedPeople/2)
+   likedPeopleArr.push(likedPeople)
+   sharedPeople=likedPeople*3
+   cummulative=likedPeople++
+  }
+  cummulative=likedPeopleArr.reduce((acc,val)=>acc+val)
+  return cummulative
+
+}
+console.log(viralAdvertising(3))
