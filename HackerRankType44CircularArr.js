1a3aad9c41934dc1860d8e5822542443f0bb2090
khushboo kumari <ksingh200601@gmail.com>
2025-01-06 21:36:20 +0530

    hackerrank questio done

---
+++ HackerRankType44CircularArr.js
@@ -0,0 +1,14 @@
+function circularArrayRotation(a, k, queries) {
+    let result=[]
+   for(let i=0;i<k;i++){
+   let removed=a.pop()
+    a.unshift(removed)
+    
+   }
+   for(let el of queries){
+     result.push(a[el])
+   }
+   return result
+
+}
+console.log(circularArrayRotation([1,2,3],2,[0,1,2]))
