023937b6db153b722ade63f1bb67fd66c2eaa2fa
khushboo kumari <ksingh200601@gmail.com>
2025-01-24 23:48:08 +0530

    modified

---
+++ hackerRAnkType47.js
@@ -0,0 +1,15 @@
+function jumpingOnClouds(c, k) {
+    let e=100;
+for(let i=0;i<c.length;i+=k){
+    if(c[i]===1){
+        e=e-1-2
+    }
+    else{
+        e=e-1
+    }
+}
+return e
+
+}
+console.log(jumpingOnClouds([1 ,1 ,1, 0 ,1 ,1, 0, 0 ,0 ,0],3))
+
