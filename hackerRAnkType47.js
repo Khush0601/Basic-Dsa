1144c4ae25e7db55af7911d95694f7a3f251e75a
khushboo kumari <ksingh200601@gmail.com>
2025-01-25 23:38:21 +0530

    hackerrank jumpingonClouds solved

--- hackerRAnkType47.js
+++ hackerRAnkType47.js
@@ -1,16 +1,14 @@
-function jumpingOnClouds(c, k) {
-    let e=100;
-for(let i=0;i<c.length;i+=k){
-    if(c[i]===1){
-        e=e-1-2
+function jumpingOnClouds(c) {
+    let count = 0;
+    for (let i = 0; i < c.length-1;) {
+        if (i + 2 < c.length && c[i + 2] === 0) {
+            i += 2;  // Jump two clouds
+        } else {
+            i += 1;  // Jump one cloud
+        }
+        count++;  // Increase jump count
     }
-    else{
-        e=e-1
-    }
+    return count;
 }
-
-return e
-
-}
-console.log(jumpingOnClouds([1 ,1 ,1, 0 ,1 ,1, 0, 0 ,0 ,0],3))
+console.log(jumpingOnClouds([0,0,0,0,1,0]))
 
