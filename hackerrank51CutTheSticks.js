3ea8ac4db0352bc3c00483039c00414256eccbfb
khushboo kumari <ksingh200601@gmail.com>
2025-02-01 18:50:56 +0530

    hackerrank cut the sticks problem

---
+++ hackerrank51CutTheSticks.js
@@ -0,0 +1,14 @@
+function cutTheSticks(arr) {
+    arr.sort((a, b) => a - b); 
+    let result = [];
+    let n = arr.length;
+    for (let i = 0; i < n; i++) {
+        if (i === 0 || arr[i] !== arr[i - 1]) { 
+            result.push(n - i);
+        }
+    }
+   return result;
+   
+ 
+}
+console.log(cutTheSticks([5,4,4,2,2,8]))
