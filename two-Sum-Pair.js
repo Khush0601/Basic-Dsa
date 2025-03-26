ace26267c49a5b8a740551abc9a0a51162cca5fa
khushboo kumari <ksingh200601@gmail.com>
2025-03-26 23:22:21 +0530

    two sum pair

---
+++ two-Sum-Pair.js
@@ -0,0 +1,14 @@
+// two sum -pair question
+function twoSum(arr,target){
+   let result=[]
+   for(let i=0;i<arr.length;i++){
+    for(let j=i+1;j<arr.length;j++){
+        if(arr[i]+arr[j]===target){
+            result.push(arr[i],arr[j])
+        }
+    }
+   
+   }
+   return result
+}
+console.log(twoSum([0,-1,2,-3,1],2))
