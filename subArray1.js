7310fa6251798491ff3147c0bbe0254b0f9408c4
khushboo kumari <ksingh200601@gmail.com>
2025-04-01 00:00:28 +0530

    sum of all the possible subarray

--- subArray1.js
+++ subArray1.js
@@ -1,10 +1,22 @@
 // print all the possible subarray
 function printSubArray(arr){
 let n=arr.length;
+const newArr=[]
+let totalsum=0
 for(let i=0;i<n;i++){
     for(let j=i;j<n;j++){
-        console.log(arr.slice(i,j+1))
+      newArr.push(arr.slice(i,j+1))
     }
 }
+
+for(let arr of newArr){
+  let sum=0;
+  for(let el of arr){
+    sum+=el;
+   
+  }
+  totalsum+=sum
 }
-printSubArray([1,2,3])
+return totalsum
+}
+console.log(printSubArray([1,2,3]))
---
+++ subArray2.js
@@ -0,0 +1,21 @@
+// sum of all the possible subarray
+function printSubArray(arr){
+    let n=arr.length;
+    const newArr=[]
+    let totalsum=0
+    for(let i=0;i<n;i++){
+        for(let j=i;j<n;j++){
+          newArr.push(arr.slice(i,j+1))
+        }
+    }
+    
+    for(let arr of newArr){
+      let sum=0;
+      for(let el of arr){
+        sum+=el;
+      }
+      totalsum+=sum
+    }
+    return totalsum
+    }
+    console.log(printSubArray([1,2,3]))
