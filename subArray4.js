5578ce47de535b54156fc646dcba1716ddd377c8
khushboo kumari <ksingh200601@gmail.com>
2025-04-01 15:39:23 +0530

    Check if a Subarray with Zero Sum Exists

--- subArray4.js
+++ subArray4.js
@@ -2,18 +2,20 @@
 
 
 function hasZeroSumSubarray(nums) {
-    for (let i = 0; i < nums.length; i++) {
-        let sum = 0;
-        
-        for (let j = i; j < nums.length; j++) {
-            sum += nums[j]; 
-            if (sum === 0) {
-                return "Yes";  
-            }
-        }
+    let result=[]
+   for(let i=0;i<nums.length;i++){
+    let sum=0;
+    let subArr=[]
+    for(let j=i;j<nums.length;j++){
+     sum+=nums[j]
+     subArr.push(nums[j])
+     if(sum===0){
+        result.push([...subArr])
+
+     }
     }
-    
-    return "No";
+   }
+    return result;
 }
 
 console.log(hasZeroSumSubarray([4, 2, -3, 1, 6])); 
