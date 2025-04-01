2c514238e103a8497bb1c48a5d9937fc2ec1a7a9
khushboo kumari <ksingh200601@gmail.com>
2025-04-01 16:06:41 +0530

    find the length of the longest subarray with a sum equal to zero

---
+++ subArr5.js
@@ -0,0 +1,26 @@
+// find the length of the longest subarray with a sum equal to zero.
+
+function sumEqualToZero(arr){
+    let result=[]
+    let maxlength=0;
+    let subArrLength=0
+    let longestSubArr=[]
+    for(let i=0;i<arr.length;i++){
+        let sum=0;
+        let subArr=[];
+        for(let j=i;j<arr.length;j++){
+            sum+=arr[j]
+            subArr.push(arr[j])
+            if(sum===0){
+               result.push([...subArr])
+               subArrLength=Math.max(maxlength,subArr.length)
+            if(subArr.length>maxlength){
+                maxlength=subArr.length;
+                longestSubArr=[...subArr]// qki copy kr rhe hai
+            }
+            }
+        }
+    }
+  return {longestSubArr,subArrLength}
+}
+console.log(sumEqualToZero([15, -2, 2, -8, 1, 7, 10, 23]))
