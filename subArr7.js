9f73ae1204a9371c534d10358cf779b8436e4e0a
khushboo kumari <ksingh200601@gmail.com>
2025-04-03 20:07:41 +0530

    find the max. product and subArr

---
+++ subArr7.js
@@ -0,0 +1,21 @@
+// find the max. product and arr
+function maxProductSubArr(arr){
+let maxProduct=1;// in case of negative int put -infinty
+let productResultArr=[];
+for(let i=0;i<arr.length;i++){
+    let product=1;//product of subArr
+    let productSubArr=[];
+    for(let j=i;j<arr.length;j++){
+        product*=arr[j];
+        productSubArr.push(arr[j])
+        if(product>maxProduct){
+            maxProduct=product;
+            productResultArr=[...productSubArr]
+        }
+    }
+
+
+}
+return {maxProduct,productResultArr}
+}
+console.log(maxProductSubArr([1,2,3,4,5]))
