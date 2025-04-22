abd274ebf81079e1f7c70b5594cf18b680225484
khushboo kumari <ksingh200601@gmail.com>
2025-04-22 17:57:46 +0530

    merge two object

--- objectques6.js
+++ objectques6.js
@@ -5,3 +5,21 @@ function mergeObjects(obj1,obj2){
 const obj1={a:1,b:2}
 const obj2={b:3,c:4}
 console.log(mergeObjects(obj1,obj2))
+
+//alternate method:
+
+function mergeObjects(obj1,obj2){
+    const merged={};
+    //copy properties from obj1:
+    for(let key in obj1){
+        merged[key]=obj1[key]
+    }
+    //copy properties of obj2:
+    for(let key in obj2){
+        merged[key]=obj2[key]
+    }
+    return merged;
+}
+const objj1={a:1,b:2}
+const objj2={b:3,c:4}
+console.log(mergeObjects(objj1,objj2))
