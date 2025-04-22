99919e02ac5662c598e581413a2fba882b99528c
khushboo kumari <ksingh200601@gmail.com>
2025-04-23 01:10:11 +0530

    merge the objects

---
+++ objectques10.js
@@ -0,0 +1,29 @@
+//
+
+let arr1=[
+    {id:1,name:'shanu'},
+    {id:2,name:'khushboo',age:23},
+    {id:2,name:'ltr',address:'sksk'}
+]
+
+let arr2=[
+    {id:1,class:1},
+    {id:3,name:'khushboo',age:23}
+]
+
+function findUnique(arr1,arr2){
+  let neww=[...arr1,...arr2];
+  
+  return neww
+}
+let mergelist=findUnique(arr1,arr2)
+let obj={}
+mergelist.forEach((el)=>{
+    if(obj[el.id]){
+      obj[el.id]={...obj[el.id],...el}
+    }
+    else{
+        obj[el.id]=el
+    }
+})
+console.log(Object.values(obj))
