80f9d8f793627d35cf477d1ea31eb87fccf32977
khushboo kumari <ksingh200601@gmail.com>
2025-05-07 19:46:28 +0530

    convert object to array of keys or values

---
+++ objectQuest18.js
@@ -0,0 +1,10 @@
+// convert object to array of keys or values:
+
+const fruits = {
+    apple: 5,
+    banana: 10,
+    mango: 2
+};
+
+console.log(Object.keys(fruits))
+console.log(Object.values(fruits))
